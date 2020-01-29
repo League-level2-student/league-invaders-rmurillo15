e9b5de08cecf6e9474408fbb6bc943de5af78c4c
ilabstaff <ilabstaff@172.16.116.77>
2020-01-28 19:22:22 -0800

    LeagueInvaders.java - src
    GamePanel.java - src

--- src/LeagueInvaders.java
+++ src/LeagueInvaders.java
@@ -1,4 +1,32 @@
+import javax.swing.JFrame;
 
 public class LeagueInvaders {
 
+	JFrame jframe;
+	public static final int WIDTH = 500;
+	public static final int HIGHT = 800;
+	GamePanel gamepanel;
+	
+	LeagueInvaders(){
+		
+	jframe = new JFrame();
+	gamepanel = new GamePanel();
+		
+	}
+	
+	public static void main(String[] args) {
+		
+	LeagueInvaders iv = new LeagueInvaders();
+	iv.Setup();
+		
+	}
+	
+	void Setup(){
+	
+	jframe.add(gamepanel);
+	jframe.setSize(WIDTH, HIGHT);
+	jframe.setVisible(true);
+	jframe.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	
+	}
 }
