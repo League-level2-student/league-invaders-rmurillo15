826c828a9f702a73a906c28fac97af98354d139b
ilabstaff <ilabstaff@172.16.116.79>
2020-02-25 19:20:55 -0800

    GamePanel.java - src

--- src/GamePanel.java
+++ src/GamePanel.java
@@ -1,15 +1,88 @@
+import java.awt.Color;
+import java.awt.Font;
 import java.awt.Graphics;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.JPanel;
+import javax.swing.Timer;
 
-public class GamePanel extends JPanel {
+public class GamePanel extends JPanel implements ActionListener{
 
+	Font titleFont;
+	Font enterFont;
+	Font spaceFont;
+	Font gameoverFont;
+    final int MENU = 0;
+    final int GAME = 1;
+    final int END = 2;
+    int currentState = MENU;
+    Timer frameDraw;
+    
+    void updateMenuState() { 
+    	
+    }
+    void updateGameState() { 
+    	
+    }
+    void updateEndState()  { 
+    	
+    }
+    void drawMenuState(Graphics g) { 
+	
+    	g.setColor(Color.BLACK);
+    	g.fillRect(0, 0, LeagueInvaders.WIDTH, LeagueInvaders.HEIGHT);
+    	
+    	g.setFont(titleFont);
+    	g.setColor(Color.YELLOW);
+    	g.drawString("League Invaders", 60, 200);
+    	
+    	g.setFont(enterFont);
+    	g.setColor(Color.YELLOW);
+    	g.drawString("Press ENTER to start", 100, 400);
+    	
+    	g.setFont(spaceFont);
+    	g.setColor(Color.YELLOW);
+    	g.drawString(" Press SPACE for instructions", 50, 500);
+    	
+    }
+    void drawGameState(Graphics g) { 
+	
+    }
+    void drawEndState(Graphics g)  { 
+	
+    	
+    	
+    }
+	GamePanel(){
+		
+		 titleFont = new Font("Arial", Font.PLAIN, 48);
+		 gameoverFont = new Font("Arial", Font.PLAIN, 50);
+		 enterFont = new Font("Arial", Font.PLAIN, 30);
+		 spaceFont = new Font("Arial", Font.PLAIN, 30);
+	}
+	
 @Override
 public void paintComponent(Graphics g){
 
-	g.fillRect(10, 10, 100, 100);
+	if(currentState == MENU){
+		
+	    drawMenuState(g);
+	    
+	}else if(currentState == GAME){
+		
+	    drawGameState(g);
+	    
+	}else if(currentState == END){
+		
+	    drawEndState(g);
+	}
+}
+@Override
+public void actionPerformed(ActionEvent e) {
+	// TODO Auto-generated method stub
 	
 }
-	
+
 		
 }
--- src/LeagueInvaders.java
+++ src/LeagueInvaders.java
@@ -4,7 +4,7 @@ public class LeagueInvaders {
 
 	JFrame jframe;
 	public static final int WIDTH = 500;
-	public static final int HIGHT = 800;
+	public static final int HEIGHT = 800;
 	GamePanel gamepanel;
 	
 	LeagueInvaders(){
@@ -24,7 +24,7 @@ public class LeagueInvaders {
 	void Setup(){
 	
 	jframe.add(gamepanel);
-	jframe.setSize(WIDTH, HIGHT);
+	jframe.setSize(WIDTH, HEIGHT);
 	jframe.setVisible(true);
 	jframe.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	
